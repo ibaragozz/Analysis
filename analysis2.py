5627bae543e1024129138d0b5397426c9107d209
ibaragozz <baragozz1@gmail.com>
2024-10-05 12:25:15 +0300

    Update analysis2.py

---
+++ analysis2.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+# data = {
+#     'dataA' : [85, 90, 95, 100, 105],
+#     'dataB' : [70, 80, 95, 110, 120]
+# }
+# df = pd.DataFrame(data)
+# stdA = df['dataA'].std()
+# stdB = df['dataB'].std()
+# print(f'stdA: {stdA}')
+# print(f'stdB: {stdB}')
+
+# data = {
+#     'Age' : [23,22,21,27,23,20,29,28,22,25],
+#     'Salary' : [54000,58000,60000,52000,55000,59000,51000,49000,53000,61000]
+# }
+# df = pd.DataFrame(data)
+#
+# print(f'Средний возраст - {df["Age"].mean()}')
+# print(f'Медиана возраста - {df["Age"].median()}')
+# print(f'Стандартное отклонение возраста - {df["Age"].std()}')
+#
+#
+# print(f'Средняя зарплата - {df["Salary"].mean()}')
+# print(f'Медиана зарплаты - {df["Salary"].median()}')
+# print(f'Стандартное отклонение зарплаты - {df["Salary"].std()}')
+
+# print(f'Мода возраста - {df["Age"].mode()}')
+# print(f'Мода зарплаты - {df["Salary"].mode()}')
