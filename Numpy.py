77ec815ad6bddcc9a2fa67687d9f7615790be36e
ibaragozz <baragozz1@gmail.com>
2024-10-05 12:44:03 +0300

    add matplotlib

--- Numpy.py
+++ Numpy.py
@@ -1,15 +1,16 @@
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
+#
+#
+# date = pd.date_range(start='2022-07-26', periods=10, freq='D')
+# values = np.random.rand(10)
+#
+# df = pd.DataFrame({'Date': date, 'values': values})
+# df.set_index('Date', inplace=True)
+#
+# print(df)
+#
+# month = df.resample('ME').mean()
+#
+# print(month)
 
-
-date = pd.date_range(start='2022-07-26', periods=10, freq='D')
-values = np.random.rand(10)
-
-df = pd.DataFrame({'Date': date, 'values': values})
-df.set_index('Date', inplace=True)
-
-print(df)
-
-month = df.resample('ME').mean()
-
-print(month)
