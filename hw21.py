6cb3093daff952abe26209b72422482e6798718e
ibaragozz <baragozz1@gmail.com>
2024-10-05 20:58:29 +0300

    Update hw21.py

---
+++ hw21.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+data = {
+    'name': ['Alice','Bob', 'Charlie', 'David', 'Eve'],
+    'gender': ['female', 'male', 'male', 'male', 'female'],
+    'department': ['HR', 'Engineering', 'Marketing', 'Engineering', 'HR']
