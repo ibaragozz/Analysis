44cb8e226083a7a8c3f9c30ff5ddc14ae8028db4
ibaragozz <baragozz1@gmail.com>
2024-10-05 13:07:32 +0300

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -1,2 +1,12 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
+
+data = {'value': [1, 2, 3, 3, 3, 4, 4, 4, 5, 6, 7, 8, 9, 10, 55]}
+
+df = pd.DataFrame(data)
+
+# df['value'].hist()
+# df = boxplot(column='value')
+df.boxplot(column='value')
+
+plt.show()
