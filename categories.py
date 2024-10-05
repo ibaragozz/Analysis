0f0f3dfb17e33fa33a746419560b9a9ad998fd36
ibaragozz <baragozz1@gmail.com>
2024-10-05 13:48:05 +0300

    Create categories.py

---
+++ categories.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+data = {
+    'name': ['Alice','Bob', 'Charlie', 'David', 'Eve'],
+    'gender': ['female', 'male', 'male', 'male', 'female'],
+    'department': ['HR', 'Engineering', 'Marketing', 'Engineering', 'HR']
+}
+df = pd.DataFrame(data)
+
