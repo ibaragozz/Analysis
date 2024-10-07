b26b0a8a6857316aec8025bbbbd3d1e90c5ad6bf
ibaragozz <baragozz1@gmail.com>
2024-10-07 11:58:46 +0300

    hw_22_done

--- hw22.py
+++ hw22.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 
 # Задание №1
 
-# data = np.random.normal(0,1,1000)
-# plt.hist(data)
-# plt.show()
+data = np.random.normal(0,1,1000)
+plt.hist(data)
+plt.show()
 
 # Задание №2
 
-# x = np.random.rand(5)
-# y = np.random.randn(5)
-# plt.scatter(x, y)
-# plt.show()
+x = np.random.rand(5)
+y = np.random.randn(5)
+plt.scatter(x, y)
+plt.show()
